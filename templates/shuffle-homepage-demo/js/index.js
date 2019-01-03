2b74c1e25fa115f7fffed51b57cc95e5fb3cdcf6
juliafredenburg <julia.fredenburg@gmail.com>
2019-01-03 17:56:48 -0500

    new explorer

--- templates/shuffle-homepage-demo/js/index.js
+++ templates/shuffle-homepage-demo/js/index.js
@@ -144,7 +144,20 @@ Demo = function () {
       });
     } }]);return Demo;}();
 
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
 
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    };
 document.addEventListener('DOMContentLoaded', function () {
   window.demo = new Demo(document.getElementById('grid'));
 });
