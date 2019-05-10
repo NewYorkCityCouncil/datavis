a21f00669592d8d564f6f4f8953b59fac3f08f85
romartinez-nycc <44377052+romartinez-nycc@users.noreply.github.com>
2019-05-10 13:57:20 -0400

    not needed

--- templates/dropdown.js
+++
@@ -1,9 +0,0 @@
- jQuery(document).on('change', '.div-toggle', function() {
-  var target = jQuery(this).data('target');
-  var show = jQuery("option:selected", this).data('show');
-  jQuery(target).children().addClass('hide');
-  jQuery(show).removeClass('hide');
-});
-jQuery(document).ready(function(){
-	jQuery('.div-toggle').trigger('change');
-});
