440d16a242a0ef91d7179aae13d4fab6c3209f25
Julia Fredenburg <julia.fredenburg@gmail.com>
2021-03-12 17:06:40 -0600

    added real data to boxplot

--- templates/boxplot02/chart-js-boxplot/dist/script.js
+++ templates/boxplot02/chart-js-boxplot/dist/script.js
@@ -1,42 +1,54 @@
 function randomValues(count, min, max) {
   const delta = max - min;
-  return Array.from({length: count}).map(() => Math.random() * delta + min);
+  return Array.from({length: count}).map(() => delta + min);
 }
+
+function randomBoxPlot(min, max) {
+  const values = randomValues(5, min, max).sort((a, b) => a - b);
+
+  return {
+    min: 18,
+    q1: 34,
+    median: 42,
+    q3: 52,
+    max: 92
+  };
+}
+
 
 const boxplotData = {
   // define label tree
-  labels: ('Workers'),
+  labels: ['Workers'],
   datasets: [{
     label: 'Dataset 1',
-    backgroundColor: 'rgba(255,0,0,0.5)',
-    borderColor: 'red',
+    backgroundColor: '#2F56A6',
+    borderColor: '#23417D',
     borderWidth: 1,
     outlierColor: '#999999',
     padding: 10,
     itemRadius: 0,
+    outlierColor: '#999999',
     data: [
-      18,
-      92,
-      34,
-      52,
-      42,
-      49
+      randomBoxPlot(0, 100)
     ]
   }]
-};
+}
+
 window.onload = () => {
   const ctx = document.getElementById("canvas").getContext("2d");
   window.myBar = new Chart(ctx, {
-    type: 'boxplot',
+    type: 'horizontalBoxplot',
     data: boxplotData,
     options: {
       responsive: true,
       legend: {
+        display: false,
         position: 'top',
       },
       title: {
         display: true,
-        text: 'Chart.js Box Plot Chart'
+        text: 'Age (Years)',
+        position: 'bottom',
       }
     }
   });
